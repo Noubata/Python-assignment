d78c42ce5a602a47b4103be9e7acd57f428651f1
Noubata Benoit <benynouba@gmail.com>
2025-09-21 14:41:56 +0100

    new files

--- odd_or_even.py
+++ odd_or_even.py
@@ -2,8 +2,8 @@
 
 ints = int(input("Enter an integer:"))
 
-if ints % 2 == 0:
+if ints % 2 != 0:
 
-	print(ints, 'is an even number')
-else : 
 	print(ints, 'is an odd number')
+else : 
+	print(ints, 'is an even number')
